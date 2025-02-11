de6e81c99a3700de81985c0310a87c39916be52c
tagciccone <tagciccone@gmail.com>
2025-02-11 16:36:59 -0500

    Add logging.basicConfig() details

--- src/capy_app/main.py
+++ src/capy_app/main.py
@@ -1,5 +1,8 @@
 # stl imports
 import os
+import logging
+from time import strftime, gmtime
+import socket
 
 # local imports
 from frontend.bot import Bot
@@ -9,6 +12,11 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    logging.basicConfig(filename = f'logs/{strftime("%Y-%m-%d %H:%M:%S", gmtime())}@{socket.gethostname()}.log',level=logging.INFO)
+
     bot = Bot()
     bot.run()
 
