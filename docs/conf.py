fb36e7d13bf7a06393dce7bb0f079d11bc993dc0
Jason Zhang <jasonyue10@gmail.com>
2025-02-13 16:10:12 -0500

    idk why no work

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,10 @@ from pathlib import Path
 project_root = Path(__file__).parent.parent
 src_path = project_root / "src"
 
+
 # Add paths to Python path
-sys.path.insert(0, str(src_path))
 sys.path.insert(0, str(project_root))
+sys.path.insert(0, str(src_path))
 
 
 # -- Project information -----------------------------------------------------
