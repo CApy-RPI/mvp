b90f81b2515eafa29be598a9610cb10500c28eb9
tagciccone <tagciccone@gmail.com>
2025-02-14 16:24:18 -0500

    Add proper message to console when crashing due to uncaught exception

--- src/capy_app/sys_logger.py
+++ src/capy_app/sys_logger.py
@@ -4,16 +4,21 @@ from time import strftime, gmtime
 import socket
 import sys
 
+WARNING = "\033[93m"
+FAIL = "\033[91m"
 def init_logger():
     if not os.path.exists("logs"):
         os.mkdir("logs")
 
-    logging.basicConfig(filename=f'logs/{strftime("%Y-%m-%d %H:%M:%S", gmtime())}@{socket.gethostname()}.log',
+    logfile = f'logs/{strftime("%Y-%m-%d %H:%M:%S", gmtime())}@{socket.gethostname()}.log'
+    logging.basicConfig(filename=logfile,
                         level=logging.INFO)
 
     except_logger = logging.getLogger("sys")
 
     def handler(type, value, tb):
+        print(f'{FAIL}ENCOUNTERED {type.__name__}: CHECK {logfile} FOR MORE DETAILS')
         except_logger.exception("Uncaught exception: {0}".format(str(value)), stack_info=True)
 
     sys.excepthook = handler
+
