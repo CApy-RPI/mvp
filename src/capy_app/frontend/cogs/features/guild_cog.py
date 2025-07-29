bc73fa4a885b7c077cab83b31927f40a76cd33f8
woweiseng <168587256+woweiseng@users.noreply.github.com>
2025-07-29 14:48:17 -0400

    fixed formatting issues with too many characters in one line

--- src/capy_app/frontend/cogs/features/guild_cog.py
+++ src/capy_app/frontend/cogs/features/guild_cog.py
@@ -135,7 +135,10 @@ class GuildCog(commands.Cog):
         # Show channels
         channel_text = "\n".join(
             f"{prompt['label']}: "
-            f"{'<#' + str(getattr(guild_data.channels, name)) + '>' if getattr(guild_data.channels, name) else 'Not Set'}"
+            f"{'<#'
+               + str(getattr(guild_data.channels, name))
+               + '>' if getattr(guild_data.channels, name) else 'Not Set'
+               }"
             for name, prompt in self.config.get_channel_prompts().items()
         )
         embed.add_field(
@@ -147,7 +150,10 @@ class GuildCog(commands.Cog):
         # Show roles
         role_text = "\n".join(
             f"{prompt['label']}: "
-            f"{'<@&' + str(getattr(guild_data.roles, name)) + '>' if getattr(guild_data.roles, name) else 'Not Set'}"
+            f"{'<@&'
+               + str(getattr(guild_data.roles, name))
+               + '>' if getattr(guild_data.roles, name) else 'Not Set'
+               }"
             for name, prompt in self.config.get_role_prompts().items()
         )
         embed.add_field(name="Roles", value=role_text or "No roles configured", inline=False)
